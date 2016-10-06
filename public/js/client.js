a3e2a56238bce237e49556592eab9d1405748742
codeofcolors <lily05131@hotmail.com>
2016-10-06 10:46:31 -0500

    final update

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,50 @@
-console.log('Client side js loaded');
+// console.log('Client side js loaded');
+$(function() {
+    //ask the server for songs, and then draw them
+    getSongs();
+
+    //listen for submit envents and send new songs to the server
+// for the first 3 will be post
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+
+        var formData = $(this).serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: '/songs',
+            data: formData,
+            success: getSongs
+        });
+
+        $(this).find('input[type=text]').val('');
+
+        // $('form').submit(function() {
+        //   $(this).children('.blank').remove();
+        // });
+
+
+    });
+
+
+
+});
+//the last one #4 will be GET (from assignment)
+//add a gitignore file to homework file// do not add node_modules
+function getSongs() {
+    $.ajax({
+        type: 'GET',
+        url: '/songs',
+        success: function(songs) {
+            $('#songs').empty();
+            songs.forEach(function(song) {
+                var $li = $('<li></li>');
+                $li.append('<p>' + song.title + '</p>');
+                $li.append('<p>by: ' + song.artist + '</p>');
+                $li.append('<p>' + 'date ' + song.dateAdded + '</p>');
+                $('#songs').append($li);
+            });
+
+        }
+    });
+}
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var path = require('path');
 
 var app = express();
 
+var date = new Date();
+
 // middleware function, gets executed on each request
 app.use(function(req, res, next){
   console.log('Got a request!');
@@ -26,14 +28,97 @@ app.get('/', function(req, res){
   res.sendFile(filename);
 });
 
-app.get('/kittens', function(req, res){
-  console.log('Query params:', req.query);
-  if (req.query.age > 2) {
-    res.send('MEOW!');
-  } else {
-    res.send('meow');
+// app.get('/kittens', function(req, res){
+//   console.log('Query params:', req.query);
+//   if (req.query.age > 2) {
+//     res.send('MEOW!');
+//   } else {
+//     res.send('meow');
+//   }
+// });
+
+
+var songs = [];
+app.post('/songs', function(req, res){
+console.log('req.body:', req.body);
+var candidateSong = req.body;
+// songs.push(req.body);
+console.log('songs', songs);
+
+
+
+
+var duplicate = false;
+var empty = false;
+
+
+
+songs.forEach(function(song){  //function to not allow the user to add duplicate songs.
+  console.log('song in loop', song); //testing
+
+if(candidateSong.artist == song.artist) {
+  if(candidateSong.title == song.title) {
+  duplicate = true;
   }
+}
+
 });
+
+if(duplicate == true) {
+  res.sendStatus(400);
+}else{
+
+  if(req.body.title == ""){
+    if(req.body.artist == ""){
+      empty = true;
+
+    }
+  }
+if(empty ==true) {
+  res.sendStatus(400);
+
+}else{
+
+  console.log('pushing', req.body);
+  candidateSong.dateAdded = date.toDateString();
+  songs.push(req.body);
+  res.sendStatus(200);
+}
+
+}
+
+
+// songs.forEach(function(song){  //function to not allow the user to add songs with a blank artist or title field.
+//   if(req.body.title == ""){
+//     if(req.body.artist == ""){
+//       empty = true;
+//
+//     }
+//   }
+// if(empty ==true) {
+//   res.sendStatus(400);
+//
+// }
+
+
+
+
+});
+
+
+
+
+
+
+
+app.get('/songs', function(req, res){
+  res.send(songs);
+});
+
+
+
+
+
 
 // middleware for serving static files
 app.use(express.static('public'));
